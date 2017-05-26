9c26a822d192e471f5ad0fa076b2f389b198ede8
Francisco Romano <s171872@ad.aie.edu>
2017-05-26 22:13:09 +1000

    Testing Islands

---
+++ Project_Wave/Assets/generation/IslandGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class IslandGenerator : MonoBehaviour {
+
+	public int minIslands = 3;
+	public int maxIslands = 5;
+	public List<GameObject> islands;
+
+	void Awake()
+	{
+		// generete number of islands
+		int size = (int)(Random.value * (float)(this.maxIslands - this.minIslands)) + this.minIslands;
+		// iterate throught each island
+		for (int i = 0; i < size; i++)
+		{
+			// generate random positions
+			float tx = (Random.value - 0.5f) * 10;
+			float ty = (Random.value - 0.5f) * 10;
+			// select model to instantiate
+			//int index = (int)Random.Range(0, islands.Count - 1);
+			int index = 0;
+			// create new island object
+			GameObject obj = Instantiate(islands[index], new Vector3(tx, ty, 0), new Quaternion(), this.transform);
+		}	
+	}
+
+}
